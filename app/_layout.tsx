c23b2058d2c35b789953c5a9f72bb88b80359952
delamamba <105841105422@student.unismuh.ac.id>
2025-07-31 11:35:56 +0800

    Tugas 8

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,11 +2,41 @@ import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { TouchableOpacity, View } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    return(
-        <Stack />
-    )
+//   return ( <Stack screenOptions= {{ 
+//     header : ({navigation}) => (
+
+//     <View style={{
+//         height : 60,
+//         backgroundColor: '#f8f8f8',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//     }}>
+//         <TouchableOpacity style={{
+//             width: 60,
+//             marginLeft: 10,
+//         }}>
+//             <AntDesign name="home" size={24} color="black" />
+//         </TouchableOpacity>
+//     </View>
+//     )
+//   }}>
+// </Stack>
+//   );
+// }
+return (
+    <Stack>
+        <Stack.Screen 
+            name="(tabs)"
+            options={{ 
+                headerShown: false,
+            }}
+        />
+        </Stack>
+    );
 }
